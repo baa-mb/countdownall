f4c669423e30899de7fde1db4ae410fe2a8dbb2a
baa <34665120+baa-mb@users.noreply.github.com>
2021-05-25 23:40:43 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,51 @@
+input.onButtonPressed(Button.A, function () {
+    countDownZahl(60, 1000)
+})
+input.onButtonPressed(Button.B, function () {
+    countDownGraph(64, 1000)
+})
+function countDownZahl (zeit: number, pause2: number) {
+    for (let Index = 0; Index <= zeit; Index++) {
+        anzeige = zeit - Index
+        zehner = Math.round(anzeige / 10 - 0.5)
+        einer = anzeige % 10
+        DLPLmatrix.schreibeText(
+        nrMatrix.zentral,
+        convertToText(zehner),
+        0x007fff,
+        false
+        )
+        DLPLmatrix.schreibeText(
+        nrMatrix.links,
+        convertToText(einer),
+        0x007fff,
+        false
+        )
+        if (anzeige < 5) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+        } else {
+            basic.pause(pause2)
+        }
+    }
+}
+function countDownGraph (pixelZahl: number, pause2: number) {
+    streifen = neopixel.create(DigitalPin.P1, pixelZahl, NeoPixelMode.RGB)
+    streifen.clear()
+    streifen.showColor(neopixel.colors(NeoPixelColors.White))
+    streifen.show()
+    for (let Index = 0; Index <= pixelZahl; Index++) {
+        streifen.setPixelColor(Index, neopixel.colors(NeoPixelColors.Red))
+        streifen.show()
+        if (anzeige < 5) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+        } else {
+            basic.pause(pause2)
+        }
+    }
+}
+let streifen: neopixel.Strip = null
+let einer = 0
+let zehner = 0
+let anzeige = 0
+DLPLmatrix.definitionMatrix(nrMatrix.zentral, HWMatrix.m2, 1)
+DLPLmatrix.definitionMatrix(nrMatrix.links, HWMatrix.m1, 2)
